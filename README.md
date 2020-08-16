611548c50c6fb70a4f1da320a8a9e1a8afbbf332
Nick Beattie <nick@nickbytes.com>
2020-08-16 13:43:34 -0700

    Testing testing mostly copying other peoples' code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# slowmovie-shots
+# slowdrive-in
 
 Upload dithered screenshots of movies to instagram.
 
-That's it. That's the whole thing.
+Most of this code is from https://github.com/TomWhitwell/SlowMovie but I've adapted it to upload photos to Instagram.
--- main.py
+++ main.py
@@ -18,13 +18,38 @@ def generate_frame(in_filename, out_filename, time, width, height):
 # Ensure this is the correct path to your video folder
 viddir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Videos/")
 
-while 1:
 
-    # Pick a random .mp4 video in your video directory
-    currentVideo = ""
-    while not (currentVideo.endswith('.mp4')):
-        videoCount = len(os.listdir(viddir))
-        randomVideo = random.randint(0, videoCount-1)
-        currentVideo = os.listdir(viddir)[randomVideo]
-    inputVid = viddir + currentVideo
-    print(inputVid)
+# Pick a random .mp4 video in your video directory
+currentVideo = ""
+while not (currentVideo.endswith(".mp4")):
+    videoCount = len(os.listdir(viddir))
+    randomVideo = random.randint(0, videoCount - 1)
+    currentVideo = os.listdir(viddir)[randomVideo]
+inputVid = viddir + currentVideo
+print(inputVid)
+
+# Ensure this matches your particular screen or desired dimensions
+width = 800
+height = 480
+
+# Check how many frames are in the movie
+frameCount = int(ffmpeg.probe(inputVid)["streams"][0]["nb_frames"])
+
+# Pick a random frame
+frame = random.randint(0, frameCount)
+
+# Convert that frame to Timecode
+msTimecode = "%dms" % (frame * 41.666666)
+
+# Use ffmpeg to extract a frame from the movie, crop it, letterbox it and save it as grab.jpg
+generate_frame(inputVid, "grab.jpg", msTimecode, width, height)
+
+# Open grab.jpg in PIL
+pil_im = Image.open("grab.jpg")
+
+# Dither the image into a 1 bit bitmap (Just zeros and ones)
+pil_im = pil_im.convert(mode="1", dither=Image.FLOYDSTEINBERG)
+
+print("Diplaying frame %d of %s" % (frame, currentVideo))
+
+pil_im.save(fp='grab.jpg')
